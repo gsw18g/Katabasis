cd5ddad3c95dc2b32c1be61c83ea710dc8f99b4c
tarkovguy2057 <111811631+tarkovguy2057@users.noreply.github.com>
2022-10-13 20:03:26 -0400

    fixes jump
    fixes jump

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -150,10 +150,7 @@ public class player_movement : MonoBehaviour
 
 
         }
-        else
-        {
-            animator.SetBool("jump", false);
-        }
+       
         /*
          * if(slope_check)
         {
@@ -235,6 +232,12 @@ public class player_movement : MonoBehaviour
         Debug.Log("reset stab = false");
         animator.SetBool("stab", false);
     }
-   
+
+
+    public void reset_jump()
+    {
+        
+        animator.SetBool("jump", false);
+    }
 
 }
