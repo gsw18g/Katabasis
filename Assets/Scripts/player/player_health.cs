f2e2e5726b14acb7b3d96141112b449a2ca84224
tarkovguy2057 <111811631+tarkovguy2057@users.noreply.github.com>
2022-11-19 15:40:38 -0500

    re organize player prefab
    re organize player prefab

--- Assets/Scripts/player/player_health.cs
+++ Assets/Scripts/player/player_health.cs
@@ -8,48 +8,30 @@ public class player_health : MonoBehaviour
     public static int health;
     bool player_dead;
     float timer;
-    float knock_time = 0f;
     public static int num_hearts;
-    //Vector2 knockback;
-    Rigidbody2D rb;
-    float velocity;
-    float speed;
-    float mod;
-    float pos_y;
-    public static bool knock;
+    public static bool knock = false;
     bool bat_melee;
 
     // Start is called before the first frame update
     void Start()
     {
-        take_damage = enemy_melee.melee;
+        //take_damage = enemy_melee.melee;
         health = 100;
         player_dead = false;
         timer = 0f;
         num_hearts = 3;
-        //knockback = new Vector3(11f, 0f, 0f);
-        rb = GetComponent<Rigidbody2D>();
-
-        speed = 10f;
-        mod = .2f;
-        velocity = 0f;// gameObject.transform.position.x + (speed * Time.deltaTime * mod);
-        knock = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos_y = transform.position.y;
-
-
-        //Debug.Log("player_health = " + health);
 
         timer += Time.deltaTime;//timer wrong ???????
 
-        take_damage = enemy_melee.melee;
+        //take_damage = enemy_melee.melee;
         bat_melee = Behaviour.bat_melee;
 
-        if (take_damage || bat_melee)// || bat_melee
+        if (take_damage || bat_melee)
         {
             player_damage();
         }
@@ -65,15 +47,12 @@ public class player_health : MonoBehaviour
             health -= 34;
             timer = 0f;
             num_hearts -= 1;
-            //rb.AddForce(knockback, ForceMode2D.Impulse);
-
-
         }
 
         if (health <= 0)
         {
             player_dead = true;
-            //Debug.Log("Player is dead");
+
         }
     }
 
@@ -85,6 +64,7 @@ public class player_health : MonoBehaviour
 
             bat_melee = true;
         }
+
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -115,11 +95,7 @@ public class player_health : MonoBehaviour
             take_damage = true;
         }
 
-        if (collision.transform.CompareTag("bat"))
-        {
-
-            take_damage = true;
-        }
+        
     }
 
 
@@ -131,11 +107,7 @@ public class player_health : MonoBehaviour
             take_damage = true;
         }
 
-        if (collision.transform.CompareTag("bat"))
-        {
-
-            take_damage = true;
-        }
+       
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -146,11 +118,7 @@ public class player_health : MonoBehaviour
             take_damage = false;
         }
 
-        if (collision.transform.CompareTag("bat"))
-        {
-
-            take_damage = false;
-        }
+        
     }
 
 
