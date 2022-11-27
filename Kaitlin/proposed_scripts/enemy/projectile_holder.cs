5df24a194e52ffa4fc9ee72f3a1f2eafbaca1b86
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 18:21:41 -0500

    proposed changes

--- Kaitlin/proposed_scripts/enemy/projectile_holder.cs
+++ Kaitlin/proposed_scripts/enemy/projectile_holder.cs
@@ -1,3 +1,7 @@
+// projectile_holder.cs: update the projectile holder
+// written by: Gavin Williams, Matthew Kaplan
+// TODO: can this be put somewhere else if it's just one function?
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
