787320d84ce9b2ff91dad8b27900626b3457ba3c
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 17:24:50 -0500

    proposed changes

--- Kaitlin/proposed_scripts/camera/move_camera.cs
+++ Kaitlin/proposed_scripts/camera/move_camera.cs
@@ -1,3 +1,7 @@
+// camera_level.cs: Centers and moves camera with player
+// written by: Matthew Kaplan
+// TO DO: This does the same thing as camera_level2 except one value is changed, is there a better way to implement?
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,28 +16,18 @@ public class move_camera : MonoBehaviour
     //offset camera to center on player
     float offset = 0f;//8.39
     public float y_offset = 2f;
+    
     // Start is called before the first frame update
     void Start()
     {
         the_player = player.GetComponent<player_movement>();
-        //x_pos = the_player.transform.position.x;
         x_pos = gameObject.transform.position.x;
         y_pos = gameObject.transform.position.y;
-        //y_pos = the_player.transform.position.y;
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-        //x_pos = gameObject.transform.position.x;
-
         gameObject.transform.position = new Vector3(x_pos + the_player.transform.position.x + offset, y_pos + the_player.transform.position.y + y_offset, 0f);
-
-        //player.transform.position.x to get x component 
-
-        //gameObject.transform.position = new Vector3(x_pos, y_pos, 0f);
-
-
     }
 }
