bee2e60a9da01636d00b1af84dbd1820c8ccf3a6
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 18:11:06 -0500

    proposed changes

--- Kaitlin/proposed_scripts/enemy/enemy_projectile.cs
+++ Kaitlin/proposed_scripts/enemy/enemy_projectile.cs
@@ -1,19 +1,22 @@
+// enemy_projectile.cs: Activates, moves, and deactivates arrow when it hits an object
+// written by: Gavin Williams
+
 using UnityEngine;
 
-public class EnemyProjectile : EnemyDamage
+public class enemy_projectile : EnemyDamage
 {
     [SerializeField] private float speed;
-    [SerializeField] private float resetTime;
+    [SerializeField] private float reset_time;
     private float lifetime;
     private Animator anim;
-    private BoxCollider2D boxCollider;
+    private BoxCollider2D box_collider;
 
     private bool hit;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        box_collider = GetComponent<BoxCollider2D>();
     }
 
     public void ActivateProjectile()
@@ -21,25 +24,26 @@ public class EnemyProjectile : EnemyDamage
         hit = false;
         lifetime = 0;
         gameObject.SetActive(true);
-        boxCollider.enabled = true;
+        box_collider.enabled = true;
     }
+    
     private void Update()
     {
         if (hit) return;
-        float movementSpeed = speed * Time.deltaTime;
-        transform.Translate(movementSpeed, 0, 0);
+        float movement_speed = speed * Time.deltaTime;
+        transform.Translate(movement_speed, 0, 0);
 
         lifetime += Time.deltaTime;
-        if (lifetime > resetTime)
+        if (lifetime > reset_time)
             gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //base.OnTriggerEnter2D(collision); 
         hit = true;
-        boxCollider.enabled = false;
+        box_collider.enabled = false;
         Debug.Log(collision.tag);
+        
         if (collision.transform.CompareTag("Player"))
         {
             collision.GetComponent<player_health>().player_damage();
@@ -47,11 +51,11 @@ public class EnemyProjectile : EnemyDamage
         else if (collision.transform.CompareTag("sword_check"))
         {
             GameObject obj = collision.gameObject;
-            GameObject parentObj = obj.transform.parent.gameObject;
-            parentObj.GetComponent<player_health>().player_damage();
+            GameObject parent_obj = obj.transform.parent.gameObject;
+            parent_obj.GetComponent<player_health>().player_damage();
         }
 
-        gameObject.SetActive(false); //When this hits any object deactivate arrow
+        gameObject.SetActive(false); // When this hits any object deactivate arrow
     }
 
     private void Deactivate()
