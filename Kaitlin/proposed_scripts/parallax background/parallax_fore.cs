6f19839f997dafc3eb3e4b836bb9fdab719cad17
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 18:45:07 -0500

    proposed changes

--- Kaitlin/proposed_scripts/parallax background/parallax_fore.cs
+++ Kaitlin/proposed_scripts/parallax background/parallax_fore.cs
@@ -1,3 +1,6 @@
+// parallax_fore.cs: Move foremost layer of background at certain rate for illusion of depth
+// written by: Matthew Kaplan
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +14,7 @@ public class parallax_fore : MonoBehaviour
     float x_pos;
     float y_pos;
     float offset;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +22,13 @@ public class parallax_fore : MonoBehaviour
         the_player = player.GetComponent<player_movement>();
         x_pos = gameObject.transform.position.x;
         y_pos = gameObject.transform.position.y;
-        //vel = gameObject.transform.position.x;
         offset = transform.position.x;
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //vel = the_player.velocity;
-
         vel = the_player.transform.position.x;
-
-        //vel = vel - (x_pos.transform.position.x * speed);
-        //gameObject.transform.position = new Vector3(x_pos + (-vel * speed), y_pos, 0f);
         gameObject.transform.position = new Vector3((-vel * speed) + offset, y_pos, 0f);
-
-        //Debug.Log("player vel = " + vel);
     }
 }
