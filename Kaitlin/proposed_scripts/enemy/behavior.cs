10127cbba693ad1ffda0fbf9a78a838b8a2824a8
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 17:54:31 -0500

    proposed changes

--- Kaitlin/proposed_scripts/enemy/behavior.cs
+++ Kaitlin/proposed_scripts/enemy/behavior.cs
@@ -1,17 +1,21 @@
+// behavior.cs: Bat enemy behavior to follow and “hit” player and check if bat has been hit by player
+// written by: Matthew Kaplan
+// TODO: is this one being used anymore?
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Behaviour : MonoBehaviour
+public class behavior : MonoBehaviour
 {
-	public Transform player;
-	public float moveSpeed = 5f;
-	private Vector2 movement;
-	private Rigidbody2D rb;
-    //bool to check if player should be knocked back and damaged
-	public static bool bat_melee;
+    public Transform player;
+    public float move_speed = 5f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    // bool to check if player should be knocked back and damaged
+    public static bool bat_melee;
     private bool hit_bat = false;
-    //adds offset so the bat isnt right on top of the player
+    // adds offset so the bat isnt right on top of the player
     private Vector3 offset;
 
     // Start is called before the first frame update
@@ -24,29 +28,31 @@ public class Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //adds offset in the x direction to put a little space between the bat and player
-       movement=player.position-transform.position + offset;
-	   movement.Normalize();
+        // adds offset in the x direction to put a little space between the bat and player
+        movement = player.position - transform.position + offset;
+	movement.Normalize();
 
         if(hit_bat && Input.GetMouseButtonDown(0))
         {
             Destroy(gameObject);
         }
     }
+    
+    private void FixedUpdate()
+    {
+	moveCharacter(movement);
+    }
 
-	private void FixedUpdate(){
-		moveCharacter(movement);
-	
-	}
-
-	void moveCharacter(Vector2 dir){
-		rb.MovePosition((Vector2)transform.position+(dir*moveSpeed*Time.deltaTime));
-	
-	}
+    void moveCharacter(Vector2 dir)
+    {
+	rb.MovePosition((Vector2)transform.position + (dir * move_speed * Time.deltaTime));
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //check if should do player knockback and damage
-        if (collision.transform.CompareTag("Player"))
+    
+    	if (collision.transform.CompareTag("Player"))
         {
             bat_melee = true;
         }
@@ -55,9 +61,7 @@ public class Behaviour : MonoBehaviour
         if (collision.transform.CompareTag("sword_check"))
         {
             hit_bat = true;
-        }
-
-      
+        }      
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -87,11 +91,4 @@ public class Behaviour : MonoBehaviour
             hit_bat = false;
         }
     }
-
-
-
-
-
-
-
 }
