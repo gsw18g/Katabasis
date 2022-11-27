c7b6f09cde0f5b4bf71dbe920e331ce2b45e3030
Kaitlin Tran <54950007+ktran41@users.noreply.github.com>
2022-11-27 18:01:22 -0500

    Update enemy_melee.cs

--- Kaitlin/proposed_scripts/enemy/enemy_melee.cs
+++ Kaitlin/proposed_scripts/enemy/enemy_melee.cs
@@ -1,3 +1,7 @@
+// enemy_melee.cs: Zombie behavior when it sees player and takes damage
+// written by: Gavin Williams, Matthew Kaplan
+// TODO: get_health() and set_health()
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,60 +24,52 @@ public class enemy_melee : MonoBehaviour
     [SerializeField] private float knockbackvl=10f;
     [SerializeField] private float knockbacktime = 1f;
 
-    
-
     // Start is called before the first frame update
     void Start()
     {
-        //assign player
+        // assign player
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
- 
     }
 
     // Update is called once per frame
     void Update()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-
         // Move our position a step closer to the target.
         var step = speed * Time.deltaTime; // calculate distance to move
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
-        //apply move toward player to rigidbody
+        // apply move toward player to rigidbody
         rb.transform.position = transform.position;
 
-        //if left mouse pressed and take_damage == true (enemy has been hit with sword)
+        // if left mouse pressed and take_damage == true (enemy has been hit with sword)
         if (Input.GetMouseButtonDown(0) && take_damage)
         {
-            //apply damage to enemy
+            // apply damage to enemy
             enemy_damage();
-            
         }
-
     }
 
     void enemy_damage()
     {
-        //if health is positive substract 34 and knockback enemy
+        // if health is positive substract 34 and knockback enemy
         if (health > 0)
         {
             health -= 34;
             knockback(); 
         }
-        //if enemy has 0 health play death animation
+        
+        // if enemy has 0 health play death animation
         if (health < 0)
         {
             health = 0;
-            //animator.SetBool("start_death", true);
-
-            //spawn zombie death animation where zombie died
+            // spawn zombie death animation where zombie died
             Instantiate(zombie_death, new Vector3(transform.position.x, transform.position.y, 0f), Quaternion.identity);
             Destroy(gameObject);
         }
-
     }
 
-    //at end of death animation destroy enemy gameobject
+    // at end of death animation destroy enemy gameobject
     public void end_death_anim()
     {
         Destroy(gameObject);
@@ -83,8 +79,6 @@ public class enemy_melee : MonoBehaviour
     {
         if(collision.transform.CompareTag("Player"))
         {
-
-
             melee = true;
         }
 
@@ -98,8 +92,6 @@ public class enemy_melee : MonoBehaviour
     {
         if (collision.transform.CompareTag("Player"))
         {
-
-
             melee = true;
         }
 
@@ -113,8 +105,6 @@ public class enemy_melee : MonoBehaviour
     {
         if (collision.transform.CompareTag("Player"))
         {
-
-
             melee = false;
         }
 
@@ -136,18 +126,4 @@ public class enemy_melee : MonoBehaviour
     {
         yield return new WaitForSeconds(knockbacktime);
     }
-
-    /*
-     * override protected int get_health()
-    {
-
-        return this.health;
-    }
-
-    override protected void set_health()
-    {
-        this.health -= 34;
-    }
-     * */
-
 }
